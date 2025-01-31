ac374cb4740711eec51e5295ce408c0c7292f083
Shmbles <eadr1995@gmail.com>
2025-01-31 12:28:14 -0500

    Initial commit

---
+++ app/src/main/java/com/azrael/to_docompose/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.azrael.to_docompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.azrael.to_docompose.ui.theme.ToDoComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToDoComposeTheme {
+
+            }
+        }
+    }
+}
