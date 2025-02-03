8c0bd1349d808aa6dd30e9282a9790f07ae421b0
Shmbles <eadr1995@gmail.com>
2025-02-02 21:19:39 -0500

    Set room database

---
+++ app/src/main/java/com/azrael/to_docompose/data/ToDoDatabase.kt
@@ -0,0 +1,10 @@
+package com.azrael.to_docompose.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.azrael.to_docompose.data.model.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+}
